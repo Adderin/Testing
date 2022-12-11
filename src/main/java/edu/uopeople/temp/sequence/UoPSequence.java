4963caf5ebf64dd1a5334ad1aed1463f8f6f3433
Adel Ibragimov <adderin@gmail.com>
2022-12-11 15:20:37 +0100

    First commit; add negative numbers sequence

---
+++ src/main/java/edu/uopeople/temp/sequence/UoPSequence.java
@@ -0,0 +1,53 @@
+package edu.uopeople.temp.sequence;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class UoPSequence {
+
+    public static void main(String[] args) {
+        List<List<Integer>> sequences = new ArrayList<>();
+
+        createNumbers()
+                .forEach(number -> sequences.add(createSequence(number)));
+
+        showSequences(sequences);
+    }
+
+    private static List<Integer> createSequence(int number) {
+        List<Integer> sequence = new ArrayList<>();
+        sequence.add(number);
+
+        do {
+            number = updateNumber(number);
+            sequence.add(number);
+        }
+        while (number < -1);
+        return sequence;
+    }
+
+    private static void showSequences(List<List<Integer>> sequences) {
+        sequences.forEach(System.out::println);
+    }
+
+    private static int updateNumber(int number) {
+        if (isEven(number)) {
+            return number / 2;
+        } else {
+            return number * 3 - 1;
+        }
+    }
+
+    private static List<Integer> createNumbers() {
+        return new Random()
+                .ints(5, -100, -2)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isEven(int number) {
+        return (number ^ 1) > number;
+    }
+}
